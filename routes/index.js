65ac2c933050f22e4404a7d46a2c712680d0a46d
mdheri <mdheri@uci.edu>
2018-01-22 06:15:41 -0800

    added user sessions

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var mongoose = require('mongoose');
+var User = require('../models/user');
+var pagesModel = require('../models/page');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -12,18 +14,6 @@ router.get('/auth', function(req, res, next) {
 });
 
 
-
-
-
-
-var userSchema = mongoose.Schema({
-    email: String,
-    password: String 
-});
-
-var User = mongoose.model('User', userSchema);
-
-
 router.post('/auth/register', function(req, res, next) {
   	
   	var newuser = new User({ email: req.body.email , password:req.body.password  });
@@ -31,24 +21,44 @@ router.post('/auth/register', function(req, res, next) {
 	newuser.save(function (err, user) {
 		if (err) return console.error(err);
 	});
+
 	res.redirect("/admin");
 	res.end();
 
 });
 
-router.post('/auth/login', function(req, res, next) {
-	console.log("hello");
-  	
-
-  	mongoose.User.find({email:res.body.email},function (err, user) {
-  		if(user.password == res.body.password)res.redirect("/admin");
+router.post('/auth/login', function(req, res) { 
+  	User.findOne({email:req.body.email},function (err, user) {
+  		
+		if(err) return res.redirect('/auth');
+  		if(user != null && user.password == req.body.password){
+  			req.session.user = user;
+  			return res.redirect("/admin");
+		}
+		return res.redirect("/auth");
+		res.end();
 
 	});
-	res.redirect("/auth");
 
-	res.end();
 	
 
+});
+
+router.get('/logout', function(req, res) {
+	req.session.reset();
+	res.redirect('/');
+});
+
+router.get('/:page', function(req, res) {
+	pagesModel.findOne({url:req.params.page.trim()}, function(err,page){
+		if(err) return res.send(err);
+		if(page){
+		res.render('newpagetemplate', { title: page.title, content: page.content,useremail:page.useremail});
+		}
+		else{
+			res.send("404 error, Page not Found");
+		}
+	});
 });
 
 
